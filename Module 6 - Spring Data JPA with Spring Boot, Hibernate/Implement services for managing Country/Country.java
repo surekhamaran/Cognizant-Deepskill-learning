3a800d49d139b21495bb56537c807b4d160f7c66
surekha maran <153985261+surekhamaran@users.noreply.github.com>
2025-07-06 22:19:36 +0530

    Create Country.java

---
+++ Module 6 - Spring Data JPA with Spring Boot, Hibernate/Implement services for managing Country/Country.java
@@ -0,0 +1,32 @@
+package com.cognizant.orm_learn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    private String coCode;
+
+    private String coName;
+
+    // Getters and setters
+    public String getCoCode() {
+        return coCode;
+    }
+
+    public void setCoCode(String coCode) {
+        this.coCode = coCode;
+    }
+
+    public String getCoName() {
+        return coName;
+    }
+
+    public void setCoName(String coName) {
+        this.coName = coName;
+    }
+}
