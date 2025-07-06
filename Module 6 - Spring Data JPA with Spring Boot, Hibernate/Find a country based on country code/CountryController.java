68553c5e48dbc24d7ecfc7c2d657643190058371
surekha maran <153985261+surekhamaran@users.noreply.github.com>
2025-07-06 22:23:09 +0530

    Create CountryController.java

---
+++ Module 6 - Spring Data JPA with Spring Boot, Hibernate/Find a country based on country code/CountryController.java
@@ -0,0 +1,21 @@
+package com.cognizant.orm_learn.controller;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @GetMapping
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
